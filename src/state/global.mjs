6fc9793d6065b24c5b85fab8e400f080fc974e6d
Tyler Gillispie <tyler.gillispie@noaa.gov>
2022-04-22 16:37:56 -0400

    Initial Commit

---
+++ src/state/global.mjs
@@ -0,0 +1,17 @@
+/**
+ * Generic Data Model
+ */
+const data = {
+  requestType: `fetch`,
+  file: null,
+  // Misc. method for loading a file attachement.
+  emailAttchmentUrl: '',
+
+  // API Parameters
+  // OARS
+  // oarsPath: `https://apps-nefsc.fisheries.noaa.gov/oars/`,
+  // oarsPath: `http://localhost:3005`,
+  oarsKey: `Paste key here w/ \r\n linebreaks:  Example key starts with '-----BEGIN PUBLIC KEY-----\r\n' and ends with '\r\n-----END PUBLIC KEY-----'`,
+}
+
+export { data }
